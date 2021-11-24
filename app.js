178950934985010be8161c1728c26463a106dd6f
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2021-11-24 05:37:45 -0700

    redirect requests to index.html with sendFile

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const { join } = require("path");
 express.static("./public");
 const app = express();
 const port = 8800;
-app.use(express.static("./todo-reactjs/build"));
+const staticDir = "./todo-reactjs/build";
+app.use(express.static(staticDir));
+
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, staticDir) + "/index.html");
+});
 
 app.listen(port, () => {
   console.log(`listening on http://localhost:/${port}`);

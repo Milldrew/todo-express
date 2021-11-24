4fe96aeb3791070fc265ea784103bc195f6b6c9e
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2021-11-24 06:11:55 -0700

    added PORT env var

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { join } = require("path");
 express.static("./public");
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 const staticDir = "./build";
 app.use(express.static(staticDir));
 
